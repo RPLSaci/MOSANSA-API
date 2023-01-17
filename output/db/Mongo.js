0cb002af3ec5934a07f4e9c7733cc82ad5fe6664
MoonLGH <MoonLMC1212@gmail.com>
2023-01-17 05:32:22 +0000

    rm

--- output/db/Mongo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export async function connect(url) {
-    await mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-    return;
-}
